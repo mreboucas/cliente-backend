0068cf53be612b5ae7816fcaac6d65822365a9e2
Marcelo da Costa Rebouças <marcelo.reboucas@ciccr-ce.interno>
2020-09-20 20:36:58 -0300

    Cliente Resource

--- src/main/java/br/com/luizalabs/cliente/v1/resource/ClienteResource.java
+++ src/main/java/br/com/luizalabs/cliente/v1/resource/ClienteResource.java
@@ -1,39 +1,45 @@
 package br.com.luizalabs.cliente.v1.resource;
 
-import java.util.List;
+import javax.validation.Valid;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import br.com.luizalabs.cliente.v1.model.Cliente;
-import br.com.luizalabs.produto.v1.dto.ProdutoDTO;
-import br.com.luizalabs.produto.v1.service.ProdutoService;
-import br.com.luizalabs.util.exception.PaginaProdutoInvaldaException;
+import br.com.luizalabs.cliente.v1.service.ClienteService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.SwaggerDefinition;
+import io.swagger.annotations.Tag;
+import reactor.core.publisher.Mono;
 
 /**
- * @author Marcelo Reboucas - marceloreboucas10@gmail.com - 19 de set de 2020 as 04:47:41 
+ * @author Marcelo Reboucas - marceloreboucas10@gmail.com - 20 de set de 2020 as 10:23:06
  */
+@Api(tags = {"Clientes"}, description = "Manutenção de clientes")
+@SwaggerDefinition(tags = {@Tag(name = "Clientes")})
 @RestController
-@RequestMapping("v1/cliente")
+@RequestMapping("v1/clientes")
 public class ClienteResource {
 
-	private ProdutoService produtoService;
+	private ClienteService clientService;
 
-	public ClienteResource(ProdutoService produtoService) {
+	public ClienteResource(ClienteService clientService) {
 		super();
-		this.produtoService = produtoService;
+		this.clientService = clientService;
 	}
 	
+
+	@ApiOperation(value = "Salva um cliente", notes = "Responsável por inserir na base um cliente")
+	@ApiResponses({@ApiResponse(code = 201, message = "Success")})
 	@ResponseStatus(code = HttpStatus.CREATED)
-	private void salvar(Cliente cliente) {
-		
-	}
-	
-	@GetMapping("/produtos/{pagina}")
-	public List<ProdutoDTO> listProdutos(@PathVariable("pagina") int pagina) throws PaginaProdutoInvaldaException {
-		return produtoService.consultarProdutoPorPagina(pagina);
-		
+	@PostMapping
+	public Mono<Void> salvar(@RequestBody @Valid Cliente cliente) {
+		this.clientService.salvar(cliente);
+		return Mono.empty();
 	}
 }
